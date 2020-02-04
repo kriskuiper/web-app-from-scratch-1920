608f401f300ce37c7196f503c811c4f5cb061b4f
Kris <kriskuiper74@gmail.com>
2020-02-04 15:44:22 +0100

    chore: run a build

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,20 +1,85 @@
 (function () {
 	'use strict';
 
-	class LaunchItem {
-		constructor({ mission_name, launch_site, launch_success, launch_date_utc }) {
-			this.name = mission_name,
-			this.site = launch_site.site_name_long,
-			this.isSuccess = launch_success,
-			this.launchDate = launch_date_utc;
+	class RouterLink {
+		constructor(to, text) {
+			this.to = to;
+			this.text = text;
 		}
 
 		render() {
 			return `
-			<article ${maybeSetDataSuccess(this.successState)}>
-				<h3>${this.name}</h3>
-				<p>${this.site}</p>
-				<p>${formatSuccessState(this.isSuccess)}</p>
+			<a href="${this.to}" data-router-link>${this.text}</a>
+		`
+		}
+	}
+
+	var formatDate = date => {
+		const parsedDate = new Date(date);
+		const day = maybePrefixWithZero(parsedDate.getDay());
+		const month = maybePrefixWithZero(parsedDate.getDay());
+		const year = parsedDate.getFullYear();
+
+		return `${day}-${month}-${year}`
+	};
+
+	function maybePrefixWithZero(num) {
+		return num < 10
+			? `0${num}`
+			: num
+	}
+
+	class LaunchItem {
+		constructor({ mission_name, launch_site, launch_success, launch_date_utc, links, rocket }) {
+			this.name = mission_name;
+			this.launchSite = launch_site.site_name_long;
+			this.isSuccess = launch_success;
+			this.rocketName = rocket.rocket_name;
+			this.launchDate = launch_date_utc;
+			this.imageSrcSet = [
+				`${links.mission_patch_small} 400w`,
+				`${links.mission_patch} 800w`
+			].join(', ');
+			this.fallbackImage = links.mission_patch;
+			this.imageSizes = [
+				// TODO: add image sizes
+			];
+		}
+
+		render() {
+			return `
+			<article
+				class="launch-item"
+				${maybeSetDataSuccess(this.isSuccess)}
+			>
+				<header class="launch-item__header">
+					<img
+						class="launch-item__icon"
+						src=${getIcon(this.isSuccess)}
+					/>
+					<div>
+						<h3 class="launch-item__title">${this.name}</h3>
+						<p class="launch-item__date">Launched: ${formatDate(this.launchDate)}</p>
+					</div>
+				</header>
+				<div class="fixed-ratio fixed-ratio-1by1">
+					<div class="fixed-ratio-content">
+						<img
+							class="launch-item__image"
+							src="${this.fallbackImage}"
+							srcset="${this.imageSrcSet}"
+							sizes="${this.imageSizes}"
+							alt="${this.name} mission logo"
+						/>
+					</div>
+				</div>
+				<div class="launch-item__details">
+					<ul class="unstyled-list">
+						<li>Rocket: ${this.rocketName}</li>
+						<li>Launch site: ${this.launchSite}</li>
+					</ul>
+				</div>
+				${new RouterLink('/detail', 'Details').render()}
 			</article>
 		`
 		}
@@ -24,10 +89,11 @@
 		return successState ? 'data-success' : ''
 	}
 
-	function formatSuccessState(successState) {
-		return successState === null
-			? 'unknown'
-			: successState
+	function getIcon(successState) {
+		const baseUrl = '/assets/icons/';
+		const iconName = successState ? 'check.svg' : 'warning.svg';
+
+		return `${baseUrl}${iconName}`
 	}
 
 	class LaunchList {
@@ -36,11 +102,15 @@
 		}
 
 		render() {
-			return this.items
-				.map(item => {
-					return new LaunchItem(item).render()
-				})
-				.join('')
+			return `
+			<div class="launch-list">
+				${this.items
+					.map(item => {
+						return new LaunchItem(item).render()
+					})
+					.join('')}
+			</div>
+		`
 		}
 	}
 
