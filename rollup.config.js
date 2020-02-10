6d7a04a184890e8db0458d9cc83fc9cb44887247
Kris <kriskuiper74@gmail.com>
2020-02-10 20:24:43 +0100

    feat: run a build while logging things in the worker

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,29 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 
-export default {
-	input: 'src/main.js',
-	output: {
-		file: 'docs/js/main.js',
-		format: 'iife',
+export default [
+	{
+		input: 'src/main.js',
+		output: {
+			file: 'docs/js/main.js',
+			format: 'iife',
+		},
+		plugins: [
+			resolve({
+				main: true,
+				browser: true
+			}),
+			commonjs(),
+		]
 	},
-	input: 'src/worker.js',
-	output: {
-		file: 'docs/js/worker.js',
-		format: 'iife',
-	},
-	plugins: [
-		resolve({
-			main: true,
-			browser: true
-		}),
-		commonjs(),
-	]
-}
+	{
+		input: 'src/worker.js',
+		output: {
+			file: 'docs/js/worker.js',
+			format: 'iife',
+		},
+		plugins: [
+			resolve()
+		]
+	}
+]
