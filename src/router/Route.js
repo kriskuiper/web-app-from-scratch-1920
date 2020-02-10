4b7d06028fbc2349e1f8f6d5ae7a5d380f2a90ff
Kris <kriskuiper74@gmail.com>
2020-02-10 12:41:56 +0100

    refactor(router): put link, route and routerview in components

---
+++ src/router/Route.js
@@ -0,0 +1,10 @@
+export default class Route {
+	constructor(pathname, component) {
+		this.pathname = `#${pathname}`
+		this.component = component
+	}
+
+	render() {
+		return this.component.render()
+	}
+}
