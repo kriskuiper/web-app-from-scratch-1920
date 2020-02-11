0da462908f7e50f396c5287cf7b25379d8726989
Kris <kriskuiper74@gmail.com>
2020-02-11 12:35:11 +0100

    feat(Home): check if store setting is working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,21 @@ class Home extends Component {
 		super({ store })
 	}
 
+	renderLaunches(launches) {
+		if (!launches) return 'Loading launches...'
+
+		return launches.map(launch => {
+			return `
+				<h2>${launch.flight_number}</h2>
+			`
+		})
+	}
+
 	render() {
 		return `
 			<main>
 				<h1>Home page</h1>
+				${console.log(this.props.store.state)}
 				${new RouterLink({ to: 'detail', text: 'Detail page' }).render()}
 			</main>
 		`
