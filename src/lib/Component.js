79fe45dcdfb509a206a70d07d5680cecd12a5632
Kris <kriskuiper74@gmail.com>
2020-02-15 15:09:18 +0100

    feat: add comments to component

--- src/lib/Component.js
+++ src/lib/Component.js
@@ -1,5 +1,16 @@
 import Store from '../store/Store'
 
+/*
+ * Every component should have:
+ * el: el(tag)
+ * update method: update(data) {} // what to do with updated data etc.
+ * render method: on initial render
+ *
+ * call update on state change with store.state as param
+*/
+
+
+
 export default class Component {
 	constructor(props) {
 		this.props = props
