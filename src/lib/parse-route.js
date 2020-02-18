5a76e4d9fccc1ed71f20db06385103e18d07fb44
Kris <kriskuiper74@gmail.com>
2020-02-18 15:00:04 +0100

    feat(parse-route): parse hash routes as normal routes

--- src/lib/parse-route.js
+++ src/lib/parse-route.js
@@ -16,16 +16,58 @@ const toObject = (queryString) => {
 	return {}
 }
 
-export default (route) => {
-	const pathnameRegex = new RegExp(/\/(.*)\//)
-	const queryRegex = new RegExp(/\?(.*)/)
-	const paramsRegex = new RegExp(/\/(.*)\?/)
+const stripHash = (hashRoute) => {
+	return hashRoute.replace('#/', '')
+}
 
-	console.log(route.match(pathnameRegex))
-	console.log(route.match(paramsRegex))
-	console.log(route.match(queryRegex))
+const getQueries = (route) => {
+	const queryIndex = route.indexOf('?')
+
+	if (queryIndex !== -1) {
+		return route.slice(queryIndex, route.length)
+	}
+
+	return ''
+}
+
+const getParams = (route) => {
+	// Capture a group from / to ? of a query
+	const paramsToQueryRegex = new RegExp(/\/(.*)\?/)
+
+	// Capture a group from / to the end of input
+	const paramsToEndRegex = new RegExp(/\/(.*)$/)
+
+	const matchesToQuery = route.match(paramsToQueryRegex)
+	const matchesToEnd = route.match(paramsToEndRegex)
+
+	if (matchesToQuery) {
+		return matchesToQuery[1]
+	}
+
+	else if (matchesToEnd) {
+		return matchesToEnd[1]
+	}
+
+	else {
+		return ''
+	}
+}
+
+const getPathname = (route) => {
+	const splittedRouteName = route.split('/')
+
+	return `/${splittedRouteName[0]}`
+}
+
+export default (hashRoute) => {
+	const withoutHash = stripHash(hashRoute)
+	const queries = getQueries(withoutHash)
+	const params = getParams(withoutHash)
+	const pathname = getPathname(withoutHash)
 
 	return {
-
+		pathname,
+		params,
+		queries: toObject(queries)
 	}
 }
