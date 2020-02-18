30b6c13402ce2260119c5f47450197e38d3d5da6
Kris <kriskuiper74@gmail.com>
2020-02-18 14:54:16 +0100

    refactor(router-view): just use window.onhashchange to handle route change

--- src/router/RouterView.js
+++ src/router/RouterView.js
@@ -32,27 +32,5 @@ export default class RouterView {
 				)
 			}
 		})
-
-		// Listen for router links on the page if the router isn't listening yet
-		if (!this.hasRouteListener) {
-			this.element.addEventListener('click', event => {
-				const { target } = event
-				const isRouterLink = target.getAttribute('data-router-link') !== null
-
-				if (isRouterLink) {
-					event.preventDefault()
-
-					/*
-						TODO:
-						This maybe has to be refactored as it's not really 'seperation
-						of concerns'. Now the router view is dependent on a push method
-						on the router
-					*/
-					this.router.push(target.hash)
-				}
-			})
-
-			this.hasRouteListener = true
-		}
 	}
 }
