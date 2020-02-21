53283e05756ac3a72826597995436e282ceb0b57
Kris <kriskuiper74@gmail.com>
2020-02-21 11:40:33 +0100

    feat(app): move getting home data to the home page

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import Home from './pages/Home'
 import Detail from './pages/Detail'
 
 import Store from './store'
-import { SET_DATA } from './store/mutation-keys'
 
 export default class App {
 	constructor({ target }) {
@@ -16,21 +15,10 @@ export default class App {
 			new Route('/detail', Detail)
 		)
 		this.target = document.querySelector(target)
-		this.element = this.router.view.element,
-		this.store = Store
+		this.element = this.router.view.element
 	}
 
 	async init() {
-		if (window.Worker) {
-			const apiWorker = Comlink.wrap(new Worker('js/api-worker.js'))
-			const Api = await new apiWorker
-
-			Api.getLaunches()
-				.then(launches => {
-					this.store.commit(SET_DATA, { launches })
-				})
-		}
-
 		this.target.appendChild(this.element)
 	}
 }
