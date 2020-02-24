34c37b9a419360ebc9689cb22dd2ab27d596f729
Kris <kriskuiper74@gmail.com>
2020-02-24 14:30:43 +0100

    feat(details): add styling to detail page

--- src/components/Details.js
+++ src/components/Details.js
@@ -28,9 +28,30 @@ export default class Details extends Component {
 	update(state) {
 		clearChildren(this.element)
 
+		const YOUTUBE_EMBED_URL = 'https://www.youtube.com/embed/'
+
 		redom.mount(
 			this.element,
-			redom.el('h3', { textContent: state.launch.mission_name })
+			redom.el('article.detail-page',
+				redom.el('header.detail-page__header',
+					redom.el('h2.detail-page__title', { textContent: state.launch.mission_name }),
+					redom.el('p.detail-page__rocket', { textContent: `Rocket: ${state.launch.rocket.rocket_name}` })
+				),
+
+				redom.el('h3.detail-page__title', { textContent: 'Description:' }),
+				redom.el('p', { textContent: state.launch.details }),
+
+				redom.el('h3.detail-page__title', { textContent: 'Video:' }),
+				redom.el('div.detail-page__video',
+					redom.el('div.fixed-ratio.fixed-ratio-1by1',
+						redom.el('iframe.fixed-ratio-content', {
+							src: `${YOUTUBE_EMBED_URL}${state.launch.links.youtube_id}`,
+							width: '100%',
+							allowfullscreen: ''
+						})
+					),
+				)
+			)
 		)
 	}
 }
