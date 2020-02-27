b8033155475c9bc0eaac8c4498619899f84fc2eb
Kris <kriskuiper74@gmail.com>
2020-02-27 23:12:36 +0100

    fix(use-data): fix constants import

--- src/composables/use-data.js
+++ src/composables/use-data.js
@@ -1,4 +1,4 @@
-import { LAUNCHES_NAME } from '../lib/constants'
+import { LAUNCH_DATA_NAME } from '../lib/constants'
 
 import useApi from './use-api'
 import useLocalStorage from './use-local-storage'
@@ -6,7 +6,6 @@ import useLocalStorage from './use-local-storage'
 export default async ({ flightNumber, page }) => {
 	const localStorage = useLocalStorage
 	const api = await useApi()
-
 
 	/**
 	 * When fetching all data, we want to first see
@@ -16,7 +15,7 @@ export default async ({ flightNumber, page }) => {
 	if (page) {
 		return api.getLaunches(page)
 			.catch(() => {
-				return localStorage.get(LAUNCHES_NAME)
+				return localStorage.get(LAUNCH_DATA_NAME)
 			})
 	}
 
